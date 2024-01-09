5426698819098f4f71b0bc79d885dad35d0691d6
Thomas <traktuner@pm.me>
2024-01-09 11:07:01 +0100

    optimize and consolidate script

--- run-vpn.sh
+++ run-vpn.sh
@@ -1,59 +1,47 @@
 #!/bin/bash
 
-# navigate to folder containing the files
-cd /data/split-vpn/wireguard/protonvpn/
+cd /data/split-vpn/wireguard/protonvpn/ || exit
 
-# check for prerequisites
-command -v curl >/dev/null 2>&1 || { echo >&2 "curl required, but not installed. install with 'apt install curl' (debian/ubuntu) or 'yum install curl' (centos/rhel)."; exit 1; }
-command -v jq >/dev/null 2>&1 || { echo >&2 "jq required, but not installed. install with  'apt install jq' (debian/ubuntu) or 'yum install jq' (centos/rhel)."; exit 1; }
-command -v wg >/dev/null 2>&1 || { echo >&2 "wireguard required, but not installed. install with  'apt install wireguard' (debian/ubuntu) or 'yum install epel-release elrepo-release' and 'yum install kmod-wireguard wireguard-tools' (centos/rhel)."; exit 1; }
+# Prerequisite checks
+for cmd in curl jq wg; do
+    if ! command -v "$cmd" &>/dev/null; then
+        echo >&2 "$cmd required, but not installed. Install with 'apt install $cmd' (Debian/Ubuntu) or 'yum install $cmd' (CentOS/RHEL)."
+        exit 1
+    fi
+done
 
-#variables - edit if you like - check ProtonVPN API before to get familiar with the naming scheme (SecureCore servers should work, too)
 apiurl="https://api.protonvpn.ch/vpn/logicals"
 entrycountry="AT"
 exitcountry="AT"
 vpntier="2"
 
-# util-function to find the correct servers
-get_server_data() {
-    response=$(curl -s $apiurl)
+get_min_load_server() {
+    local response=$(curl -s "$apiurl")
+    [ -z "$response" ] && { echo "ERROR - API not responding or not reachable"; exit 1; }
 
-    if [ -z "$response" ]; then
-        echo "ERROR - API not responding or not reachable"
-        exit 1
-    fi
+    local server_data=$(echo "$response" | jq -c '.LogicalServers[] | select(.EntryCountry == "'$entrycountry'" and .ExitCountry == "'$exitcountry'" and .Tier == '$vpntier')')
 
-    echo "$response" | jq -c '.LogicalServers[] | select(.EntryCountry == "'$entrycountry'" and .ExitCountry == "'$exitcountry'" and .Tier == '$vpntier')'
+    local min_load=101
+    local min_load_server=""
+
+    for data in $server_data; do
+        local server=$(echo "$data" | jq -r '.Name')
+        local load=$(echo "$data" | jq -r '.Load')
+
+        if (( $(echo "$load < $min_load" | bc -l) )); then
+            min_load="$load"
+            min_load_server="$server"
+        fi
+    done
+
+    [ -z "$min_load_server" ] && { echo "ERROR - server could not be extracted from the API"; exit 1; }
+    echo "$min_load_server"
 }
 
-parse_server_data() {
-    echo "$1" | jq -r '.Name,.Load' | paste -sd " " -
-}
+min_load_server=$(get_min_load_server)
 
-# query API and get server with the lowest utilization
-server_data=$(get_server_data)
-min_load=101
-min_load_server=""
+config_path="/data/split-vpn/wireguard/protonvpn/configs"
+cp "$config_path/wg0_${min_load_server}.conf" "$config_path/wg0.conf" && chmod 600 "$config_path/wg0.conf"
 
-for data in $server_data
-do
-    server=$(parse_server_data "$data")
-    load=$(echo "$server" | cut -d' ' -f2)
-    if (( $(echo "$load < $min_load" | bc -l) )); then
-        min_load=$load
-        min_load_server=$(echo "$server" | cut -d' ' -f1)
-    fi
-done
-
-# check if server got correctly extracted from the API
-if [ -z "$min_load_server" ]; then
-    echo "ERROR - server could not be extracted from the API"
-    exit 1
-fi
-
-# use the defined config file for the server and name it correctly + correct permissions
-cp /data/split-vpn/wireguard/protonvpn/configs/wg0_${min_load_server}.conf /data/split-vpn/wireguard/protonvpn/configs/wg0.conf && chmod 600 /data/split-vpn/wireguard/protonvpn/configs/wg0.conf
-
-# connect to wireguard vpn and output log
 echo "Using server $min_load_server"
-wg-quick up /data/split-vpn/wireguard/protonvpn/configs/wg0.conf | tee wireguard.log 2>&1
+wg-quick up "$config_path/wg0.conf" | tee wireguard.log 2>&1
